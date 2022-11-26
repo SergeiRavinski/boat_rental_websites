1d2344ea3bd1fad35e1cf437616af75545581b1b
sergei <ravinski.sergei@gmail.com>
2022-11-26 12:43:42 +0100

    Added active text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import HamburgerMenu from './modules/hamburgerMenu.js';
 import Wishlist from './modules/wishlist.js';
-//import Text from './modules/text.js';
+import Text from './modules/text.js';
 
 HamburgerMenu();
 Wishlist();
-//Text();
+Text();
