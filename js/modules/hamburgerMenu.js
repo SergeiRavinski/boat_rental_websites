7217f13cafe45be7b2322f6a3574b8c5bcad5750
sergei <ravinski.sergei@gmail.com>
2022-11-26 11:13:30 +0100

    Added humbureger menu

--- js/modules/hamburgerMenu.js
+++ js/modules/hamburgerMenu.js
@@ -1,3 +1,32 @@
 export default function HamburgerMenu() {
 
+	//variables
+	let visibleNavigation = false;
+
+	//query selectors
+	const menuButton = document.querySelector('.button__hamburgermenu');
+	const headerMenu = document.querySelector('.hamburger__menu');
+
+	//event listeners 
+	menuButton.addEventListener('click', handleButtonMenu);
+
+	//event handlers 
+	function handleButtonMenu() {
+		changeVisibility();
+		toggleMenu();	
+	}
+
+	//functions
+	function changeVisibility() {
+		visibleNavigation = !visibleNavigation; 
+	}
+
+	function toggleMenu() {
+		if (visibleNavigation === true) {
+			headerMenu.classList.add('hamburger__menu--visible');
+		} 
+		else {
+			headerMenu.classList.remove('hamburger__menu--visible');
+		}
+	}
 }
