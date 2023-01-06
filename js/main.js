6b580d0808e6fdbc52050c2048b4683bc2e1eda5
sergei <ravinski.sergei@gmail.com>
2023-01-06 19:12:17 +0100

    Added counter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import Header from './modules/header.js';
+import Counter from './modules/counter.js';
 import TextWithAnimation from './modules/text_with_animation.js';
 import Wishlist from './modules/wishlist.js';
 import SlideshowFirstBoatJaktar from './modules/main_section_slideshow_first_boat_jaktar.js';
@@ -6,6 +7,7 @@ import SlideshowSecondBoatAxopar from './modules/main_section_slideshow_second_b
 import SlideshowFirstBoatHallbergRassy from './modules/main_section_slideshow_third_boat_hallbergrassy.js';
 
 Header();
+Counter();
 TextWithAnimation();
 Wishlist();
 SlideshowFirstBoatJaktar();
---
+++ js/modules/counter.js
@@ -0,0 +1,30 @@
+export default function Wishlist() {
+
+	const countDown = () => {
+
+		const countDate = new Date('April 1, 2023, 00:00:00').getTime();
+		const now = new Date().getTime();
+		const gap = countDate - now;
+
+		const second = 1000;
+		const minute = second * 60;
+		const hour = minute * 60;
+		const day = hour * 24;
+
+		const textDay = Math.floor(gap / day);
+		const textHour = Math.floor((gap % day) / hour);
+		const textMinute = Math.floor((gap % hour)/ minute);
+		const textSecond = Math.floor((gap % minute) / second);
+		
+		document.querySelector('.main__main-section-counter-elements-countdown-day-days').innerText = textDay;
+		document.querySelector('.main__main-section-counter-elements-countdown-day-hours').innerText = textHour;
+		document.querySelector('.main__main-section-counter-elements-countdown-day-minutes').innerText = textMinute;
+		document.querySelector('.main__main-section-counter-elements-countdown-day-seconds').innerText = textSecond;
+
+		if (gap < 1000) {
+			document.querySelector('.main__main-section-counter').style.display = 'none';
+		}
+	}
+
+	setInterval(countDown, 1000);
+}	
